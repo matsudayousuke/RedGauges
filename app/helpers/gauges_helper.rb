10c27cb67284e72c7ce820731e651291b43c55f6
松田 洋祐 <matsudayousuke@matsudayousuke.local>
2010-09-06 23:30:54 +0900

    今週の7日間を返すメソッドをとりあえずHelperに実装。RubyのDateクラスにメソッドを移動するかどうかは今後吟味する予定

--- app/helpers/gauges_helper.rb
+++ app/helpers/gauges_helper.rb
@@ -1,2 +1,13 @@
 module GaugesHelper
+  def get_start_of_week(target)
+    target - target.wday 
+  end
+
+  def get_end_of_week(target)
+    get_start_of_week(target) + 6
+  end
+
+  def get_this_week
+    get_start_of_week(Date.today) .. get_end_of_week(Date.today)
+  end
 end
