06f6a1a622632e15d3554012f1396440fb42f964
松田 洋祐 <matsudayousuke@matsudayousuke.local>
2010-09-23 02:40:58 +0900

    issueリストを画面に表示

--- app/controllers/gauges_controller.rb
+++ app/controllers/gauges_controller.rb
@@ -1,9 +1,22 @@
 class GaugesController < ApplicationController
   unloadable
-
+  helper :sort
+  include SortHelper
+  helper :queries
+  include QueriesHelper
+  helper :issues
+  include IssuesHelper
 
   def index
     @project = Project.find(params[:project_id])
     @members = User.with_this_week_issues()
+    retrieve_query
+    sort_init(@query.sort_criteria.empty? ? [['id', 'desc']] : @query.sort_criteria)
+    sort_update(@query.sortable_columns)
+
+    if @query.valid?
+      @issues = @query.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version],
+        :order => sort_clause)
+    end
   end
 end
