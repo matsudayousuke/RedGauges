7db8a03d96390dd893789f474aa643c347d56fa1
松田 洋祐 <matsudayousuke@matsudayousuke.local>
2010-11-06 13:08:16 +0900

    1. 日付の先頭のゼロを除去。2. 曜日を表示。

--- app/helpers/gauges_helper.rb
+++ app/helpers/gauges_helper.rb
@@ -1,4 +1,5 @@
 module GaugesHelper
+
   def get_start_of_week(target)
     return target - 6 if target.wday == 0
     target - target.wday + 1
@@ -15,11 +16,25 @@ module GaugesHelper
   def issues_tip(issues)
     ret = '<table class="list">'
     issues.each do |i|
-      ret += '<tr id="tip-issue_' + i.id.to_s + '" class="' + cycle('odd', 'even') + '">'
+      ret += '<tr id="tip-issue_' + i.id.to_s + '" class="hascontextmenu ' + cycle('odd', 'even') + '">'
+      ret += '<td class="checkbox">' + check_box_tag("ids[]", i.id, false, :id => nil) + '</td>'
       ret += '<td>' + i.to_s + '</td>'
       ret += '</tr>'
     end
     ret += '</table>'
+  end
+
+  def wday(date)
+    wdays = [
+      l(:label_sunday),
+      l(:label_monday),
+      l(:label_tuesday),
+      l(:label_wednesday),
+      l(:label_thursday),
+      l(:label_friday),
+      l(:label_saturday)
+    ]
+    wdays[date.wday]
   end
 
 end
